2c5d5b0a1ca68cc3e6ab568ebaf33086b46e0865
jepayne1138 <jepayne1138@gmail.com>
2016-07-27 13:50:50 -0500

    Update check to use hex grid and add console command for checking.

--- spawnscan/check.py
+++ spawnscan/check.py
@@ -1,29 +1,27 @@
+from __future__ import division
 import json
-import math
+import spawnscan.stepcalculator as stepcalc
 
+
+SCAN_TIME = 4.5  # Time for a single worker to scan a single point
+M2_TO_KM2 = 1 / (stepcalc.M_IN_KM ** 2)
 
 with open('config.json') as file:
     config = json.load(file)
 
 
-def calcwork():
+def calcwork(radius=100, error=0.05):
     totalwork = 0
     area = 0
     for rect in config['work']:
-        distN = math.radians(rect[0] - rect[2]) * 6371
-        distE = math.radians(rect[3] - rect[1]) * 6371 * math.cos(math.radians((rect[0] + rect[2]) * 0.5))
-        dlat = 0.6 * 0.00225
-        dlng = dlat / math.cos(math.radians((rect[0] + rect[2]) * 0.5))
-        latSteps = int((((rect[0] - rect[2])) / dlat) + 0.75199999)
-        if latSteps < 1:
-            latSteps = 1
-        lngSteps = int((((rect[3] - rect[1])) / dlng) + 0.75199999)
-        if lngSteps < 1:
-            lngSteps = 1
-        totalwork += latSteps * lngSteps
-        area += distN * distE
-    return totalwork, area
-
-
-tscans, tarea = calcwork()
-print('total of {} steps covering {} km^2, approx {} seconds for scan'.format(tscans, tarea, tscans / (4.5 * len(config['users']))))
+        area += stepcalc.approx_area(*rect) * M2_TO_KM2
+        totalwork += len(
+            stepcalc.hex_grid(*rect, radius=radius, error=error)
+        )
+    print(
+        (
+            'Total of {} steps covering {} km^2, approx {} seconds for scan'
+        ).format(
+            totalwork, area, totalwork / (SCAN_TIME * len(config['users']))
+        )
+    )
--- spawnscan/console.py
+++ spawnscan/console.py
@@ -2,11 +2,24 @@ import argparse
 import sys
 
 import spawnscan.spawn as spawn
+import spawnscan.check as check
 
 
 def parse_arguments(args):
     parser = argparse.ArgumentParser(
         description='A simple and fast spawnPoint finder for pokemon go'
+    )
+    parser.add_argument(
+        '-c', '--check', action='store_true',
+        help='Check estimated scan time.'
+    )
+    parser.add_argument(
+        '-r', '--radius', type=float, default=100.0,
+        help='Radius of each scan point (in meters).'
+    )
+    parser.add_argument(
+        '-e', '--error', type=float, default=0.05,
+        help='Percent to overlap values to correct for any error.'
     )
     # TODO: Add any command line arguments
     return parser.parse_args(args)
@@ -15,5 +28,8 @@ def parse_arguments(args):
 def main():
     args = parse_arguments(sys.argv[1:])
 
-    # Handle the console input
-    spawn.main()
+    if args.check:
+        check.calcwork(radius=args.radius, error=args.error)
+    else:
+        # Handle the console input
+        spawn.main()
