97d74e70a4b357eae2a955567f1c308fb7e866da
jepayne <jepayne1138@gmail.com>
2016-07-26 22:53:19 -0500

    Remove bundled PGoApi package and replace with dependency.
    Remove the entire PGoApi package from being distributed through
    source control. Replace with single listed dependency in the
    requirement.txt file. Additionally, add a setup.py file for easier
    installation and setup of this package, and pull dependencies from
    the now updated requirement.txt file.
    
    As this fork is a proposal for modifications to the upstream
    repository, I used the original title, description, and author
    from the original repo to populate this package metadata.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+setup_dir = os.path.dirname(os.path.realpath(__file__))
+path_req = os.path.join(setup_dir, 'requirements.txt')
+install_reqs = parse_requirements(path_req, session=False)
+
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='spawnScan',
+    author='TBTerra',
+    description='A simple and fast spawnPoint finder for pokemon go',
+    version='1.1.0',
+    url='https://github.com/TBTerra/spawnScan',
+    packages=find_packages(),
+    install_requires=reqs
+)
