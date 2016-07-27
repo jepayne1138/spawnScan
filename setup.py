734bab27d41033ebc027ff8ae395b371be057628
jepayne <jepayne1138@gmail.com>
2016-07-26 23:19:01 -0500

    Change README to reST and include in setup.py as long description.
    Change REAME to reST as it makes distributing on PyPI oh so
    slightly easier if that is in the future.

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@ setup_dir = os.path.dirname(os.path.realpath(__file__))
 path_req = os.path.join(setup_dir, 'requirements.txt')
 install_reqs = parse_requirements(path_req, session=False)
 
+# Get the long description from the README file
+with open(os.path.join(setup_dir, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='spawnScan',
     author='TBTerra',
     description='A simple and fast spawnPoint finder for pokemon go',
+    long_description=long_description,
     version='0.1.1',
     url='https://github.com/TBTerra/spawnScan',
     packages=find_packages(),
